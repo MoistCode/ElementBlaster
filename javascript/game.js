82399415d958100602273b36db51d4fe5d396ada
Tommy Pham <phamtommylong1991@gmail.com>
2018-04-14 16:39:29 -0700

    simple canvas

---
+++ javascript/game.js
@@ -0,0 +1,5 @@
+const gameCanvas = document.getElementById('game');
+const gameContext = gameCanvas.getContext('2d');
+
+gameContext.fillStyle = '#000';
+gameContext.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
